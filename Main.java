78528eeb721a0211a91234f247209e13a48e3083
nppedronp@outlook.com <nppedronp@outlook.com>
2023-04-28 02:28:14 -0300

    priceFuncion for products

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main{
+    
+    public static double priceFunction(int i) {
+        return 50 + (450.0 * Math.sin((Math.PI * i) / 15.0));
+    }
+    
     public static void main(String[] args) {   
         System.out.println("Hello World!");
     }
