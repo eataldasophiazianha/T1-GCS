d54d78ff9a550f221fbc9998ba88ba9f629a63c1
leonardolforner <leonardoforner@gmail.com>
2023-05-05 18:40:14 -0300

    tk-3 completa

--- src/Call.java
+++ src/Call.java
@@ -1,80 +1,93 @@
-// package src;
+ package src;
 
-// import java.util.Date;
+ import java.util.Date;
 
-// public class Call {
-//     private int id;
-//     private String employerId; 
-//     private String productId; 
-//     private Date callDate;
-//     private String description;
-//     private Status status;
+ public class Call {
+     private int id;
+     private String employerId; 
+     private String productId; 
+     private Date callDate;
+     private String description;
+     private Status status;
 
-//     public Call(int id, String employerId, String productId, Date callDate, String description, String status) {
-//         this.id = id;
-//         this.employerId = employerId;
-//         this.productId = productId;
-//         this.callDate = callDate;
-//         this.description = description;
-//         this.status = Status.valueOf(status);
-//     }
-//     public enum Status {
-//     aberto,
-//     andamento,
-//     concluido,
-//     }
-//     public int getId() {
-//         return id;
-//     }
-//     public void setId(int id) {
-//         this.id = id;
-//     }
+     public Call(int id, String employerId, String productId, Date callDate, String description, String status) {
+         this.id = id;
+         this.employerId = employerId;
+         this.productId = productId;
+         this.callDate = callDate;
+         this.description = description;
+         this.status = Status.valueOf(status);
+     }
+     public enum Status {
+     aberto,
+     andamento,
+     concluido,
+     }
+     public int getId() {
+         return id;
+     }
+     public void setId(int id) {
+         this.id = id;
+     }
 
-//     public String getCaller() {
-//         return employerId;
-//     }
-//     public void setCaller(String employerId) {
-//         this.employerId = employerId;
-//     }
-//     public String getEquipment() {
-//         return productId;
-//     }
-//     public void setEquipment(String productId) {
-//         this.productId = productId;
-//     }
+     public String getCaller() {
+         return employerId;
+     }
+     public void setCaller(String employerId) {
+         this.employerId = employerId;
+     }
+     public String getEquipment() {
+         return productId;
+     }
+     public void setEquipment(String productId) {
+         this.productId = productId;
+     }
 
-//     public Date getCallDate() {
-//         return callDate;
-//     }
-//     public void setCallDate(Date callDate) {
-//         this.callDate = callDate;
-//     }
+     public Date getCallDate() {
+         return callDate;
+     }
+     public void setCallDate(Date callDate) {
+         this.callDate = callDate;
+     }
 
-//     public String getDescription() {
-//         return description;
-//     }
+     public String getDescription() {
+         return description;
+     }
 
-//     public void setDescription(String description) {
-//         this.description = description;
-//     }
+     public void setDescription(String description) {
+         this.description = description;
+     }
 
-//     public Enum getStatus() {
-//         return status;
-//     }
-//     
-//     @Override
-//     public String toString() {
-//         return "Call{" +
-//                 "id=" + id +
-//                 ", employerId='" + employerId + '\'' +
-//                 ", productId='" + productId + '\'' +
-//                 ", callDate=" + callDate +
-//                 ", description='" + description + '\'' +
-//                 ", status=" + status +
-//                 '}';
-//     }
+     public Enum getStatus() {
+         return status;
+     }
+        public void setStatus(Enum status) {
+            status = status.toLowerCase()
+                if(this.status == Status.aberto && status == Status.concluido){
+                    throw new IllegalArgumentException("Cannot close an open call");
+                }
+                if(this.status == Status.concluido && status == Status.aberto){
+                    throw new IllegalArgumentException("Cannot open a closed call");
+                }
+               if(this.status == Status.concluido && status == Status.andamento){
+                    throw new IllegalArgumentException("Cannot reopen a closed call");
+                }
+                this.status = (Status) status;
+            }
+     
+     @Override
+     public String toString() {
+         return "Call{" +
+                 "id=" + id +
+                 ", employerId='" + employerId + '\'' +
+                 ", productId='" + productId + '\'' +
+                 ", callDate=" + callDate +
+                 ", description='" + description + '\'' +
+                 ", status=" + status +
+                 '}';
+     }
  
-// }
+ }
 
 
 
