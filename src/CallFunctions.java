927fda04f0be09729bddccfec348f17e9e444ec1
Arthur Testa <arthurtesta@gmail.com>
2023-05-05 18:28:59 -0300

    add mergeCall function

---
+++ src/CallFunctions.java
@@ -0,0 +1,22 @@
+/* package src;
+import java.util.*;
+
+public class CallFunctions {
+
+    public void mergeCall(Call call) {
+
+        String caller = call.getCaller();
+
+        int callId = call.getId();
+
+        Date callDate = call.getCallDate();
+
+        String equipment = call.getEquipment();
+
+        this.setDescription(this.description + "\n Call ID#" + callId + "by user" + caller + "on" + callDate + "for equipment" + equipment + "was merged into this one.")
+        
+        call.setStatus(concluido);
+    }
+
+}
+ */
