8d8c7242eb2b10349a903a5e8c0061800062a058
Ÿuri Lima <yuricastilhoz@icloud.com>
2023-05-05 18:41:46 -0300

    func fecha todos chamados

--- src/Tk9.java
+++ src/Tk9.java
@@ -4,8 +4,8 @@
 
 //     public void closaAllCalls() {
 //         for(Call call : calls){
-//             if(call.getStatus() == "Aberto" || call.getStatus() == "Em andamento"){
-//                 call.setStatus("Concuido");
+//             if(call.getStatus() == "call.aberto" || call.getStatus() == "call.andamento"){
+//                 call.setStatus("call.concluido");
 //             }
 //         }
 //     }
