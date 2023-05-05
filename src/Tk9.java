b0462e848716fbe0feaab28782d58d1a52455127
Ÿuri Lima <yuricastilhoz@icloud.com>
2023-05-04 23:00:13 -0300

    func que fecha todos os chamados

---
+++ src/Tk9.java
@@ -0,0 +1,12 @@
+// package src;
+
+// public class Tk9 {
+
+//     public void closaAllCalls() {
+//         for(Call call : calls){
+//             if(call.getStatus() == "Aberto" || call.getStatus() == "Em andamento"){
+//                 call.setStatus("Concuido");
+//             }
+//         }
+//     }
+// }
